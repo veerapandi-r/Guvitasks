c78e7ab6e7ca85215b69ec81ff21ea19cccb6bcf
Veerapandi <63182979+veerapandi-r@users.noreply.github.com>
2020-11-16 21:23:30 +0530

    Update printallvalues.js

--- week1/JavaScript Practice problems/printallvalues.js
+++ week1/JavaScript Practice problems/printallvalues.js
@@ -1,7 +1,10 @@
 function printAllValues(obj) {
+    var arr=[]
     for (var key in obj) { 
-    console.log(key, obj[key])
+    //console.log(key, obj[key])
+    arr.push(obj[key])
   }
+  console.log(arr)
 }
 var obj = {name : "RajiniKanth", age : 33, hasPets : false}
 printAllValues(obj)
